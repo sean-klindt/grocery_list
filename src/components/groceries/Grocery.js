6e2c0c4f57fb2ebf96269381a8105ca48760da39
Sean Klindt <sk.klindt@gmail.com>
2021-09-20 17:54:23 -0600

    working delete function

--- src/components/groceries/Grocery.js
+++ src/components/groceries/Grocery.js
@@ -2,18 +2,22 @@ import React from 'react';
 
 const Grocery = ({ id, product, price, complete, groceryClick, deleteItem }) => {
   const handleDelete = () => {
-    deleteItem(product.id);
+    deleteItem(id);
   };
 
   return(
+    <>
     <li
       style={ complete ? { ...styles.grocery, ...styles.complete } : styles.grocery }
       onClick={ () => groceryClick(id) }
     >
       { product } - ${ price } 
       {/* <button onClick={onRemove(id)}>Delete</button> */}
-      <button onClick={handleDelete}>Remove</button>
     </li>
+      <button onClick={handleDelete}>Remove</button>
+      <br/>
+      <br/>
+      </>
     );
   };
 
